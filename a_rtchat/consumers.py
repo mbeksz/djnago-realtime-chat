ca76893e8a1cb53ceb9e3b60a21680539887159d
mbeksz <mustaafabatun@gmail.com>
2024-09-28 21:19:41 +0300

    ana kodlar

---
+++ a_rtchat/consumers.py
@@ -0,0 +1,92 @@
+from channels.generic.websocket import WebsocketConsumer
+from django.shortcuts import get_object_or_404
+from django.template.loader import render_to_string
+from asgiref.sync import async_to_sync
+from .models import *
+import json
+
+class ChatroomConsumer(WebsocketConsumer):
+    def connect(self):
+        self.user = self.scope['user']
+        self.chatroom_name = self.scope['url_route']['kwargs']['chatroom_name']
+        self.chatroom = get_object_or_404(ChatGroup, group_name=self.chatroom_name)
+        
+        # Kanalı gruba ekleyin
+        async_to_sync(self.channel_layer.group_add)(
+            self.chatroom_name, self.channel_name
+        )
+        
+        # Kullanıcıyı çevrimiçi listeye ekle
+        if self.user not in self.chatroom.users_online.all():
+            self.chatroom.users_online.add(self.user)
+            self.update_online_count()  # Çevrimiçi kullanıcı sayısını güncelle
+        
+        self.accept()
+        
+    def disconnect(self, close_code):
+        # Kanalı gruptan çıkarın
+        async_to_sync(self.channel_layer.group_discard)(
+            self.chatroom_name, self.channel_name
+        )
+        
+        # Kullanıcıyı çevrimiçi listeden çıkarın
+        if self.user in self.chatroom.users_online.all():
+            self.chatroom.users_online.remove(self.user)
+            self.update_online_count()  # Çevrimiçi kullanıcı sayısını güncelle
+        
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        body = text_data_json['body']
+        
+        # Mesajı veritabanına kaydet
+        message = GroupMessage.objects.create(
+            body=body,
+            author=self.user,
+            group=self.chatroom
+        )
+        
+        event = {
+            'type': 'message_handler',
+            'message_id': message.id
+        }
+        
+        # Mesajı gruba gönder
+        async_to_sync(self.channel_layer.group_send)(
+            self.chatroom_name, event
+        )
+        
+    def message_handler(self, event):
+        message_id = event['message_id']
+        message = GroupMessage.objects.get(id=message_id)
+        
+        context = {
+            'message': message,
+            'user': self.user
+        }
+        
+        # Mesajı HTML'e dönüştür ve gönder
+        html = render_to_string("a_rtchat/partials/chat_message_p.html", context=context)
+        self.send(text_data=html)
+        
+    def update_online_count(self):
+        # Çevrimiçi kullanıcı sayısını güncelle
+        online_count = self.chatroom.users_online.count() -1
+        print(f"Online kullanıcı sayısı: {online_count}")
+        
+        event = {
+            'type': 'online_count_handler',
+            'online_count': online_count
+        }
+        
+        # Gruba çevrimiçi kullanıcı sayısını gönder
+        async_to_sync(self.channel_layer.group_send)(
+            self.chatroom_name, event
+        )
+        
+    def online_count_handler(self, event):
+        # Gelen çevrimiçi kullanıcı sayısını al
+        online_count = event['online_count']
+        
+        # Çevrimiçi kullanıcı sayısını HTML'e dönüştür ve gönder
+        html = render_to_string("a_rtchat/partials/online_count.html", {'online_count': online_count})
+        self.send(text_data=html)
